1f9944181ab18c3e42cd0a7de8ff01a47d15d9dd
cdufour <opusidea@gmail.com>
2022-04-29 12:26:35 +0200

    Revert "square route"
    This reverts commit 8131e81b14d22925294faeba56faa63447519f56.

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ app.get('/', (req, res) => {
   res.send('ok')
 });
 
-app.get('/square/:n', (req, res) => {
-
-  const n = parseInt(req.params.n);
-  res.send(`Le carré de ${n} est ${n*n}`);
-})
-
 app.listen(port, () => {
   console.log('Server listening port ' + port);
 })
